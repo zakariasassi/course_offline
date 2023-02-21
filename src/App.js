94448c661b1dcfc6ea034de8977de8c9d8cd2a9f
zakariasassi <zakaria.sassi1096@gmail.com>
2023-02-21 17:47:38 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
+import Navbar from "./components/Navbar"
+import Home from './pages/Home/Home';
+import Signup from './pages/Signup/Signup';
+import { BrowserRouter  , Routes , Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+
+        
+        <BrowserRouter>
+        <Navbar />
+            <Routes>
+                <Route path={'/'} element={<Home />}  />
+                <Route path={'/home'} element={<Home />}  />
+                <Route path={'/signup'} element={<Signup />}  />
+
+            </Routes>
+        </BrowserRouter>
+
+
+
+      </>
   );
 }
 
---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <>
+        <h1>New Account</h1> 
+    </>
+  )
+}
+
+export default Signup
